f073c505dc04fb639b5f5d71cb369e7f8190602c
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-15 15:13:33 +0100

    inserito ordinamento sort

--- js/main.js
+++ js/main.js
@@ -8,5 +8,6 @@ console.log("nome utente:  " + nomeUtente);
 archivioCognomi.push(nomeUtente);
 console.log("stato array dopo push: " + archivioCognomi);
 //ordino alfabeticamente
-
+var archivioCognomiOrdinati = archivioCognomi.sort();
+console.log("stato array dopo ordinamento: " + archivioCognomiOrdinati);
 //scrivo la posizione dove si trova il cognome del utente
