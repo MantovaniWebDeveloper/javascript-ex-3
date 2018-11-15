19ab584675593416e28d5d01fd6892e3ff03fa64
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-15 14:39:06 +0100

    logica scritta in italiano del main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//creo array cognomi
+
+//chiedo all'utente il congome
+
+//aggiungo il congome alla lista di cognomi
+
+//ordino alfabeticamente
+
+//scrivo la posizione dove si trova il cognome del utente
