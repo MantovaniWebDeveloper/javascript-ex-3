de674307b7606761a19b1e6fcf3a3298d0fde309
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-15 15:03:38 +0100

    creato archivio nomi, richiesto nome utente e inserito  un nuovo cognome

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 //creo array cognomi
-
+var archivioCognomi = ["Mantovani" , "Franceschini" , "Caselli"]
+console.log("array iniziale : " + archivioCognomi);
 //chiedo all'utente il congome
-
+var nomeUtente = prompt("Insersci il tuo cognome");
+console.log("nome utente:  " + nomeUtente);
 //aggiungo il congome alla lista di cognomi
-
+archivioCognomi.push(nomeUtente);
+console.log("stato array dopo push: " + archivioCognomi);
 //ordino alfabeticamente
 
 //scrivo la posizione dove si trova il cognome del utente
